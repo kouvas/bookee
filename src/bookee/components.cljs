cd86381712ee68cf17e6be729b825e7b3538a4e7
kouvas <vasilis_k+ghub@icloud.com>
2025-09-25 01:26:40 +0300

    add unique id's to service and team members items
    will be useful later for stateful routing

--- src/bookee/components.cljs
+++ src/bookee/components.cljs
@@ -23,22 +23,23 @@
   (let [details-visible? (get-in state [:ui :details-visibility? id] false)
         details-text     (if details-visible? "Hide details" "Details")]
     (css/service-card
-      [:div.service
-       [:h3 service-name]
-       [:span (str duration " min") " mins • "]
-       [:span (str (get data/currencies currency) price)]
-       (when details
-         [:span " • "
-          [:a {:on {:click [[:toggle-details id]]}}
-           (css/details-link details-text)]])
-       (when (and details details-visible?)
-         (css/details-body details))])))
+      {:id (str "service_" id)}
+      [:h3 service-name]
+      [:span (str duration " min") " mins • "]
+      [:span (str (get data/currencies currency) price)]
+      (when details
+        [:span " • "
+         [:a {:on {:click [[:toggle-details id]]}}
+          (css/details-link details-text)]])
+      (when (and details details-visible?)
+        (css/details-body details)))))
 
 (defn team-card
   [{:keys [id name surname img details services-offered] :as user} state]
   (let [details-visible? (get-in state [:ui :details-visibility? id] false)
         details-text     (if details-visible? "Hide details" "Details")]
     (css/team-card
+      {:id (str "team_member_" id)}
       [:img.team-image {:src img :alt (str name " " surname)}]
       [:div.team-content
        [:div.team-name (str name " " surname)]
